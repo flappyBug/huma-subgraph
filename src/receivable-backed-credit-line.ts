3909638f0fe97e75b7a6012484b167d12ed3e4f6
haoran <linhaoran@ft.tech>
2025-03-16 09:11:17 +0800

    impl requirements

--- src/receivable-backed-credit-line.ts
+++ src/receivable-backed-credit-line.ts
@@ -1,260 +1,70 @@
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
-  AdminChanged as AdminChangedEvent,
-  BeaconUpgraded as BeaconUpgradedEvent,
-  BillRefreshed as BillRefreshedEvent,
-  CreditClosedAfterPayOff as CreditClosedAfterPayOffEvent,
   DrawdownMade as DrawdownMadeEvent,
-  DrawdownMadeWithReceivable as DrawdownMadeWithReceivableEvent,
-  Initialized as InitializedEvent,
-  PaymentMade as PaymentMadeEvent,
-  PaymentMadeWithReceivable as PaymentMadeWithReceivableEvent,
-  PoolConfigCacheUpdated as PoolConfigCacheUpdatedEvent,
-  PoolConfigChanged as PoolConfigChangedEvent,
   PrincipalPaymentMade as PrincipalPaymentMadeEvent,
-  PrincipalPaymentMadeWithReceivable as PrincipalPaymentMadeWithReceivableEvent,
-  Upgraded as UpgradedEvent,
-} from "../generated/ReceivableBackedCreditLine/ReceivableBackedCreditLine"
+} from "../generated/ReceivableBackedCreditLine/ReceivableBackedCreditLine";
 import {
-  AdminChanged,
-  BeaconUpgraded,
-  BillRefreshed,
-  CreditClosedAfterPayOff,
   DrawdownMade,
-  DrawdownMadeWithReceivable,
-  Initialized,
-  PaymentMade,
-  PaymentMadeWithReceivable,
-  PoolConfigCacheUpdated,
-  PoolConfigChanged,
   PrincipalPaymentMade,
-  PrincipalPaymentMadeWithReceivable,
-  Upgraded,
-} from "../generated/schema"
-
-export function handleAdminChanged(event: AdminChangedEvent): void {
-  let entity = new AdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
-  let entity = new BeaconUpgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.beacon = event.params.beacon
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleBillRefreshed(event: BillRefreshedEvent): void {
-  let entity = new BillRefreshed(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.creditHash = event.params.creditHash
-  entity.newDueDate = event.params.newDueDate
-  entity.nextDue = event.params.nextDue
-  entity.totalPastDue = event.params.totalPastDue
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleCreditClosedAfterPayOff(
-  event: CreditClosedAfterPayOffEvent,
-): void {
-  let entity = new CreditClosedAfterPayOff(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.creditHash = event.params.creditHash
-  entity.by = event.params.by
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+  TotalDrawdown,
+  TotalPrincipalPayment,
+} from "../generated/schema";
 
 export function handleDrawdownMade(event: DrawdownMadeEvent): void {
   let entity = new DrawdownMade(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.borrower = event.params.borrower
-  entity.borrowAmount = event.params.borrowAmount
-  entity.netAmountToBorrower = event.params.netAmountToBorrower
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.borrower = event.params.borrower;
+  entity.borrowAmount = event.params.borrowAmount;
+  entity.netAmountToBorrower = event.params.netAmountToBorrower;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
-}
+  entity.save();
 
-export function handleDrawdownMadeWithReceivable(
-  event: DrawdownMadeWithReceivableEvent,
-): void {
-  let entity = new DrawdownMadeWithReceivable(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.borrower = event.params.borrower
-  entity.receivableId = event.params.receivableId
-  entity.amount = event.params.amount
-  entity.by = event.params.by
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePaymentMade(event: PaymentMadeEvent): void {
-  let entity = new PaymentMade(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.borrower = event.params.borrower
-  entity.payer = event.params.payer
-  entity.amount = event.params.amount
-  entity.yieldDuePaid = event.params.yieldDuePaid
-  entity.principalDuePaid = event.params.principalDuePaid
-  entity.unbilledPrincipalPaid = event.params.unbilledPrincipalPaid
-  entity.yieldPastDuePaid = event.params.yieldPastDuePaid
-  entity.lateFeePaid = event.params.lateFeePaid
-  entity.principalPastDuePaid = event.params.principalPastDuePaid
-  entity.by = event.params.by
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePaymentMadeWithReceivable(
-  event: PaymentMadeWithReceivableEvent,
-): void {
-  let entity = new PaymentMadeWithReceivable(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.borrower = event.params.borrower
-  entity.receivableId = event.params.receivableId
-  entity.amount = event.params.amount
-  entity.by = event.params.by
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePoolConfigCacheUpdated(
-  event: PoolConfigCacheUpdatedEvent,
-): void {
-  let entity = new PoolConfigCacheUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.poolConfig = event.params.poolConfig
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePoolConfigChanged(event: PoolConfigChangedEvent): void {
-  let entity = new PoolConfigChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.newPoolConfig = event.params.newPoolConfig
-  entity.oldPoolConfig = event.params.oldPoolConfig
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let totalDrawdown = TotalDrawdown.load("total");
+  if (totalDrawdown == null) {
+    totalDrawdown = new TotalDrawdown("total");
+    totalDrawdown.totalAmount = BigInt.fromI32(0);
+  }
+  totalDrawdown.totalAmount = totalDrawdown.totalAmount.plus(
+    event.params.borrowAmount
+  );
+  totalDrawdown.save();
 }
 
 export function handlePrincipalPaymentMade(
-  event: PrincipalPaymentMadeEvent,
+  event: PrincipalPaymentMadeEvent
 ): void {
   let entity = new PrincipalPaymentMade(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.borrower = event.params.borrower
-  entity.payer = event.params.payer
-  entity.amount = event.params.amount
-  entity.nextDueDate = event.params.nextDueDate
-  entity.principalDue = event.params.principalDue
-  entity.unbilledPrincipal = event.params.unbilledPrincipal
-  entity.principalDuePaid = event.params.principalDuePaid
-  entity.unbilledPrincipalPaid = event.params.unbilledPrincipalPaid
-  entity.by = event.params.by
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.borrower = event.params.borrower;
+  entity.payer = event.params.payer;
+  entity.amount = event.params.amount;
+  entity.nextDueDate = event.params.nextDueDate;
+  entity.principalDue = event.params.principalDue;
+  entity.unbilledPrincipal = event.params.unbilledPrincipal;
+  entity.principalDuePaid = event.params.principalDuePaid;
+  entity.unbilledPrincipalPaid = event.params.unbilledPrincipalPaid;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.by = event.params.by;
 
-  entity.save()
-}
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-export function handlePrincipalPaymentMadeWithReceivable(
-  event: PrincipalPaymentMadeWithReceivableEvent,
-): void {
-  let entity = new PrincipalPaymentMadeWithReceivable(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.borrower = event.params.borrower
-  entity.receivableId = event.params.receivableId
-  entity.amount = event.params.amount
-  entity.by = event.params.by
+  entity.save();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32()),
-  )
-  entity.implementation = event.params.implementation
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let totalPrincipalPayment = TotalPrincipalPayment.load("total");
+  if (totalPrincipalPayment == null) {
+    totalPrincipalPayment = new TotalPrincipalPayment("total");
+    totalPrincipalPayment.totalAmount = BigInt.fromI32(0);
+  }
+  totalPrincipalPayment.totalAmount = totalPrincipalPayment.totalAmount.plus(
+    event.params.amount
+  );
+  totalPrincipalPayment.save();
 }
--- src/tranche-vault.ts
+++ src/tranche-vault.ts
@@ -1,17 +1,68 @@
-import { LiquidityDeposited as LiquidityDepositedEvent } from "../generated/TrancheVault/TrancheVault";
-import { LiquidityDeposited } from "../generated/schema";
+import { LiquidityDeposited as LiquidityDepositedEvent } from "../generated/JuniorTrancheVault/TrancheVault";
+import { LiquidityDeposited, TotalDeposited } from "../generated/schema";
+import { BigInt } from "@graphprotocol/graph-ts";
 
-export function handleLiquidityDeposited(event: LiquidityDepositedEvent): void {
+function getTotalDeposited(): TotalDeposited {
+  let totalDeposited = TotalDeposited.load("total");
+
+  if (totalDeposited == null) {
+    totalDeposited = new TotalDeposited("total");
+    totalDeposited.juniorAmount = BigInt.fromI32(0);
+    totalDeposited.seniorAmount = BigInt.fromI32(0);
+    totalDeposited.totalAmount = BigInt.fromI32(0);
+  }
+
+  return totalDeposited;
+}
+
+export function handleJuniorLiquidityDeposited(
+  event: LiquidityDepositedEvent
+): void {
   let entity = new LiquidityDeposited(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   );
   entity.sender = event.params.sender;
   entity.assets = event.params.assets;
   entity.shares = event.params.shares;
-
+  entity.trancheType = "junior";
   entity.blockNumber = event.block.number;
   entity.blockTimestamp = event.block.timestamp;
   entity.transactionHash = event.transaction.hash;
-
   entity.save();
+
+  // update total amount
+  let totalDeposited = getTotalDeposited();
+  totalDeposited.juniorAmount = totalDeposited.juniorAmount.plus(
+    event.params.assets
+  );
+  totalDeposited.totalAmount = totalDeposited.juniorAmount.plus(
+    totalDeposited.seniorAmount
+  );
+  totalDeposited.save();
+}
+
+export function handleSeniorLiquidityDeposited(
+  event: LiquidityDepositedEvent
+): void {
+  let entity = new LiquidityDeposited(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.sender = event.params.sender;
+  entity.assets = event.params.assets;
+  entity.shares = event.params.shares;
+  entity.trancheType = "senior";
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
+
+  // update total amount
+  let totalDeposited = getTotalDeposited();
+  totalDeposited.seniorAmount = totalDeposited.seniorAmount.plus(
+    event.params.assets
+  );
+  totalDeposited.totalAmount = totalDeposited.juniorAmount.plus(
+    totalDeposited.seniorAmount
+  );
+  totalDeposited.save();
 }
